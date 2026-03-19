37138a498c02f677fcc66ab3daf27be2f80404a4
Sanyam Sethi <sethi.sanyam116@gmail.com>
2026-03-19 16:13:31 +0530

    Auto-save

--- rag2.py
+++ rag2.py
@@ -1,10 +1,9 @@
-
-
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import Chroma
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from dotenv import load_dotenv
+
 import os
 
 load_dotenv()
